6c01fcba7c135ba865d54633263253565fd1b8e6
DESKTOP-QIKB6G0\lenovo <nurulkholidah123@gmail.com>
2018-03-28 12:31:47 +0700

    Tugas 01 - 23

---
+++ application/controllers/home.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+
+<?php
+class home extends CI_Controller{
+	function index(){
+		$this->load->view('Home');
+	}
+}
+?>
---
+++ application/views/Home.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="<?php echo base_url('assets/dist/css/boostrap.min.css')?>">
+<form class="navbar-form navbar-center" role="search">
+	<div class="form-group">
+		<input type="text" class="form-control" placeholder="Username">
+	</div>
+	<div class= "form-group">
+		<input type="text" class="form-control" placeholder="Password">
+	</div>
+	<button type="submit" class="btn btn-default">submit</button>
+</form>
